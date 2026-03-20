1cf4c8748eb65459abe1067e4c13aa38d6bda917
jscUMC <jschicangana@unimayor.edu.co>
2026-03-20 11:50:08 -0500

    Cambios a todo el proyecto

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from MainWindow import MainWindow
 
 
 def main():
+    print("Aplicacion corriendo")
     root = tk.Tk()
     app = MainWindow(root)
     root.mainloop()
-
+    print("Aplicacion cerrada")
 
 if __name__ == "__main__":
     main()
